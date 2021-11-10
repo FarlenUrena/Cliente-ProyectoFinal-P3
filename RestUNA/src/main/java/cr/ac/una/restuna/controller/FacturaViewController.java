8dfdf6a3b0e61483aac05e6614cb013d598e362f
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2021-11-10 03:43:01 -0600

    FacturaView

---
+++ RestUNA/src/main/java/cr/ac/una/restuna/controller/FacturaViewController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cr.ac.una.restuna.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Farlen
+ */
+public class FacturaViewController extends Controller implements Initializable {
+
+    @FXML
+    private VBox root;
+    @FXML
+    private ToggleGroup MetodoPago;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void initialize() {
+     
+    }
+    
+}
